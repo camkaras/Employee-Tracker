785af9f711c98cc0e23e496daa9c74009c3d732b
Cameron <cameron.karasiuk@gmail.com>
2022-06-18 05:14:12 -0400

    setting up roles and employees tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,31 @@
 INSERT INTO department (name)
 VALUES
     ('Production'),
-    ('Training'),
+    ('Management'),
     ('Maintenance'),
     ('Engineering');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+    ("Production Operator", 55000, 1), 
+    ("Shift Tech", 75000, 2), 
+    ("Electrical Engineer", 135000, 3), 
+    ("Mechanical Engineer", 120000, 4), 
+    ("Team Lead", 95000, 5), 
+    ("Plant Manager", 250000, 6);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+    ("Jack", "Walker", 1),
+    ("Jimmy", "Smith", 1), 
+    ("Greg", "Marshall", 1),
+    ("Bryan", "Cooper", 1), 
+    ("Steve", "Henderson", 1), 
+    ("Jake", "Butcher", 2),
+    ("Curtis", "Conlin", 3), 
+    ("Roger", "Ryan", 3),
+    ("Matt", "Baker", 4), 
+    ("Shane", "King", 4),
+    ("Rick", "Cook", 5), 
+    ("Rachel", "Davy", 5), 
+    ("Sarah", "Johnson", 6);
