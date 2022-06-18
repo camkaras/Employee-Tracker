f89365b5ca46011b36950b3e1173ac5b0fbdc6f3
Cameron <cameron.karasiuk@gmail.com>
2022-06-18 04:08:54 -0400

    setting up server and connecting to database

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-const fs = require('fs');
+const mysql = require('mysql2');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+      // Your MySQL username,
+        user: 'root',
+      // Your MySQL password
+        password: 'ButtonsK17',
+        database: 'tracker'
+    },
+    console.log('Connected to the Employee Tracker database.')
+);
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
 app.listen(PORT, () => {
-    console.log(`Employee Tracker server now on port ${PORT}!`);
+    console.log(`Employee Tracker Server running on port ${PORT}`);
 });
